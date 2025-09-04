ff9bd27a3f45c02d6216142b0857901ead497a89
Daigo Hirata <hirata-daigo-yc@ynu.jp>
2025-09-05 02:45:50 +0900

    Modify mistake

--- src/L2MichelElectron.cc
+++ src/L2MichelElectron.cc
@@ -18,24 +18,6 @@ L2MichelElectron::L2MichelElectron(const B2SpillSummary* spill,
 
 Bool_t L2MichelElectron::SearchAroundVertex() {
   auto vertex = (B2VertexSummary*)spill_->GetTrueVertices().At(0);
-  const TVector3& vertex_pos = vertex->GetAbsolutePosition().GetValue();
-  
-  double me_hits_counter = 0;
-
-  for (auto it_hit = spill_->BeginHit(); auto hit = it_hit.Next(); ) {
-    if ( hit->GetDetectorId() != vertex_detector_ ||
-         hit->GetTrueTimeNs() < hit_time_threshold ) continue;
-    
-    double dist = (hit->GetTrueAbsolutePosition().GetValue() - vertex_pos).Mag();
-    if (dist < radius_from_vertex_) 
-      me_hits_counter++;
-  }
-
-  return (me_hits_counter >= num_required_hits_);
-}
-
-Bool_t L2MichelElectron::SearchAroundVertex() {
-  auto vertex = (B2VertexSummary*)spill_->GetTrueVertices().At(0);
   return SearchAroundPoint(vertex->GetAbsolutePosition().GetValue(), radius_from_vertex_);
 }
 
